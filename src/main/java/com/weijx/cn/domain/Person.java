f01b28fc7ebceddc7cf67f77b3a3515cf354dc13
wjx <wjx1194953703@163.com>
2020-05-24 21:50:03 +0800

    测试git上传

---
+++ src/main/java/com/weijx/cn/domain/Person.java
@@ -0,0 +1,9 @@
+package com.weijx.cn.domain;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private Integer id;
+    private String username;
+    private String userpwd;
+}
